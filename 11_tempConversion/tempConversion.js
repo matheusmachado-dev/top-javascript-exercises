12b32eb517d3c5053c3cfced01af09d886b8507f
matheusmachado-dev <matheusmc.dev@gmail.com>
2025-07-14 12:13:49 -0300

    feat: complete exercise `11_tempConversion`

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  const convertedTemp = (temp - 32) * (5 / 9);
+  return Math.floor(convertedTemp) === convertedTemp
+    ? convertedTemp
+    : parseFloat(convertedTemp.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  const convertedTemp = temp * (9 / 5) + 32;
+  return Math.floor(convertedTemp) === convertedTemp
+    ? convertedTemp
+    : parseFloat(convertedTemp.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
